c0783fa01c2045a3fdb7a10634b2360304b25c8c
Nikita Vereshchagin <nikita.vereshchagin@ansiblehealth.com>
2024-12-05 11:48:59 +0300

    Add SimpleCalculator

---
+++ src/main/java/com/zaurtregulov/spring/test/SimpleCalculator.java
@@ -0,0 +1,23 @@
+package com.zaurtregulov.spring.test;
+
+public class SimpleCalculator {
+
+    public double sumTwoNumbers(double a, double b) {
+        return a + b;
+    }
+
+    public double minusOneNumberFromAnother(double a, double b) {
+        return a - b;
+    }
+
+    public double divideOneNumberByAnother(double a, double b) {
+        if (b == 0)
+            return -1;
+        else
+            return a / b;
+    }
+
+    public double multiplyTwoNumbers(double a, double b) {
+        return a * b;
+    }
+}
